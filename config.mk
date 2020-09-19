bf06a05916fd234ee749f732da3f9665eefcbee6
jonasled <jonas@jonasled.de>
2020-09-19 17:50:59 +0200

    Create config.mk

---
+++ config.mk
@@ -0,0 +1,23 @@
+# rpi3 for Raspberry Pi 3; rpi2 for the version 2, zerow for ZeroW
+BOARD = zerow
+
+# Hardware configuration
+PLATFORM = v0-hdmiusb
+
+# Target hostname
+HOSTNAME = pikvm
+
+# ru_RU, etc. UTF-8 only
+LOCALE = en_US
+
+# See /usr/share/zoneinfo
+TIMEZONE = Europe/Moscow
+
+# For SSH root user
+ROOT_PASSWD = root
+
+# Web UI credentials: user=admin, password=<this>
+WEBUI_ADMIN_PASSWD = admin
+
+# IPMI credentials: user=admin, password=<this>
+IPMI_ADMIN_PASSWD = admin
